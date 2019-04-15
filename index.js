08acd975287e48afed7e7652e6fb5845072be320
Your Name <firnaasfir@gmail.com>
2019-04-15 11:42:32 +0530

    index

--- index.js
+++ index.js
@@ -20,49 +20,63 @@ app.listen(3000, function(req, res){
 });
 
 //search query
-app.get('/searchJobs', function(req, res){
+app.get('/searchJobs', search);
 	
-	let trackId = [];
-	req_id = uuidv4();
-	trackId.push(req_id);
-	console.log(trackId);
-	var filename = req_id + '.json';
-	console.log(filename);
-	var queryParameter = req.query;
-	request({
-    	uri: 'https://jobs.github.com/positions.json?description='+ req.query.description +'&location='+ req.query.location,
-    	
-  	},function(err, res, body){
-  		if(!err && res.statusCode === 200){
-  			let data = [];
-  			console.log(res.statusCode) 
-    		console.log(res.headers['content-type'])
-    		data.push(body);
-    		fs.writeFile(`./files/${filename}`, data, function (err) {
-  				if (err) throw err;
-  				console.log('Saved!');
-			});
+	function search(req, res){
+		let trackId = [];
+		var req_id = uuidv4();
+		module.exports = trackId;
+		trackId.push(req_id);
+		console.log(trackId);
+		var filename = req_id + '.json';
+		console.log(filename);
+		var queryParameter = req.query;
+		request({
+	    	uri: 'https://jobs.github.com/positions.json?description='+ req.query.description +'&location='+ req.query.location,
+	    	
+	  	},function(err, res, body){
+	  		if(!err && res.statusCode === 200){
+	  			let data = [];
+	  			console.log(res.statusCode) 
+	    		console.log(res.headers['content-type'])
+	    		data.push(body);
+	    		fs.writeFile(`./files/${filename}`, data, function (err) {
+	  				if (err) throw err;
+	  				console.log('Saved!');
+				});
 
-			
-  		}
-  	})
-  	res.send(`The Generated Id for your response is ${req_id}`);
-  	res.send(`The Generated Id for your response is ${trackId}`);
-  	res.send(`The Generated Id for your response is ${body}`);
-  	res.send(`The Generated Id for your response is ${filename}`);
-})
+				
+	  		}
+	  	})
+	  	res.send(`The Generated Id for your response is ${req_id}`);
+}
 
-/*app.get('/searchJobs/:searchId', function(req, res){
-	var jobId = '362b3a32-e850-4f80-b7b6-b14685ede201';
+app.get('/searchJobs/:searchId', function(req, res){
+	var jobId = require('./index');
  	if(req.params.searchId == jobId){
  		var filename = req.params.searchId + '.json';
  		fs.readFile(`./files/${filename}`, 'utf-8' ,function(err, file) {
-   			console.log(file);
    			res.send(file);
   		});
   		fs.unlink(`./files/${filename}`, function(err){
 			if (err) throw err;
 			console.log('File Deleted');
 		});
- 	}
-})*/
+		jobId.shift();
+		console.log(jobId);
+		
+	}
+})
+/*removeTrackId(jobId);
+		function removeTrackid(arr) {
+    			var what, a = arguments, L = a.length, ax;
+    			while (L > 1 && arr.length) {
+        		what = a[--L];
+        		while ((ax= arr.indexOf(what)) !== -1) {
+            	arr.splice(ax, 1);
+        		}
+    		}
+    		return arr;
+		}*/
+
+
