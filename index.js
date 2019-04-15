a329fa6a60d2e372e161eb69904ceaa994fa9f3a
Firnaa5 <46587472+Firnaa5@users.noreply.github.com>
2019-04-15 14:34:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,20 +63,11 @@ app.get('/searchJobs/:searchId', function(req, res){
 			console.log('File Deleted');
 		});
 		jobId.shift();
-		console.log(jobId);
-		
+		console.log(jobId);		
+	} else {
+		res.send('Id does not exists');
 	}
 })
-/*removeTrackId(jobId);
-		function removeTrackid(arr) {
-    			var what, a = arguments, L = a.length, ax;
-    			while (L > 1 && arr.length) {
-        		what = a[--L];
-        		while ((ax= arr.indexOf(what)) !== -1) {
-            	arr.splice(ax, 1);
-        		}
-    		}
-    		return arr;
-		}*/
+
 
 
