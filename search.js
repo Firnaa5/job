d65dc1d1ef3d818cc90f4a004dd7989d3cef0504
Your Name <firnaasfir@gmail.com>
2019-04-17 20:24:40 +0530

    index

--- search.js
+++ search.js
@@ -4,51 +4,68 @@ var fs = require('fs');
 var trackId = [];
 var reqId = uuidv4();
 var url = 'https://jobs.github.com/positions.json';
-var filename = reqId + '.json';
 
-exports.search = function (req, res){					
+
+exports.search = function (req, res){	
+		var reqId = uuidv4();
+		var filename = reqId + '.json';				
 		request(url,function(err, res, body){
-	  		if(!err && res.statusCode === 200){	
+			if(!err && res.statusCode === 200){	
 	  			fs.writeFile(`./files/${filename}`, body, function (err) {
-	  				if (err) 
+	  				if (err){ 
 	  					console.log('An Error found while saving the response');
-	  				console.log('Saved!');	  				
+	  					var index = trackId.indexOf(reqId);
+						if (index >= 0) {
+  							trackId.splice( index, 1 );
+						}
+						console.log(trackId);
+	  				} else {
+	  				console.log('Saved!');
+	  				}	  				
 				});		
-				trackId.push(reqId);		
+						
 	  		} else {
 	  			res.statusCode === 500;
 	  			err = 'Fake Error to test the API';
 	  			console.log(err);
 	  		}
-	  	})	  	
+	  	})
+		trackId.push(reqId);
 	  	res.status(200).json({
 	  		pollId: reqId,
-	  		completed: true
+	  		completed: false
 	  	});
 }
 
 exports.searchById = function (req, res){
 	var jobId = trackId.includes(req.params.searchId);
- 	if(jobId == true){
+ 	if(jobId){
+ 		var filename = req.params.searchId + '.json';
  		fs.readFile(`./files/${filename}`, 'utf-8',function(err, data) {
-    		res.status(200).json({
-    		completed: true,
-    		content: data
-    		});
-  		});
-  		fs.unlink(`./files/${filename}`, function(err){
-			if (err) throw err;
-			console.log('File Deleted');
-		});
+ 			if(err){
+ 				res.status(404).json({
+    				content: 'Error Reading the file'
+    			});
+ 			} else {
+    			res.status(200).json({
+    			completed: true,
+    			content: data
+    			});
+    			fs.unlinkSync(`./files/${filename}`);
+    			var index = trackId.indexOf(req.params.searchId);
+				if (index >= 0) {
+  					trackId.splice( index, 1 );
+				}
+				console.log(trackId);
+    		}
+  		});  		
 		
-		var index = trackId.indexOf(reqId);
-		if (index >= 0) {
-  			trackId.splice( index, 1 );
-		}
-		console.log(trackId);
+		
 		
 	} else {
-		res.send('Id does not exists');		
+		res.status(404).json({
+    		content: 'Id does not exists'
+    	});	
 	}
 }
 
